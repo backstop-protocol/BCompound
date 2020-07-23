8a91fc1568dd4245df5438cb694382f881d95b79
Jitendra Chittoda <jcchittoda@gmail.com>
2020-07-23 17:14:23 +0530

    Removed AvatarFactory and moved creation logic into Registry

--- contracts/bprotocol/Registry.sol
+++ contracts/bprotocol/Registry.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.5.16;
 
-import { IAvatarFactory } from "./interfaces/IAvatarFactory.sol";
+import { Avatar } from "./avatar/Avatar.sol";
 
 import { Ownable } from "@openzeppelin/contracts/ownership/Ownable.sol";
 
@@ -11,38 +11,47 @@ contract Registry is Ownable {
 
     // Compound Contracts
     address public comptroller;
-    // CEther contract address
+    address public comp;
     address public cEther;
     address public priceOracle;
 
     // BProtocol Contracts
-    IAvatarFactory public avatarFactory;
+    address public pool;
+    address public bComptroller;
 
     // User => avatar
     mapping (address => address) public avatars;
 
     constructor(
         address _comptroller,
+        address _comp,
         address _cEther,
         address _priceOracle,
-        address _avatarFactory
+        address _pool,
+        address _bComptroller
     )
         public
     {
-        require(_comptroller != address(0), "Comptroller address is zero");
-        require(_priceOracle != address(0), "PriceOracle address is zero");
-
         comptroller = _comptroller;
+        comp = _comp;
         cEther = _cEther;
         priceOracle = _priceOracle;
-        avatarFactory = IAvatarFactory(_avatarFactory);
+        pool = _pool;
+        bComptroller = _bComptroller;
     }
 
     function newAvatar() external returns (address) {
         require(!isAvatarExistFor(msg.sender), "Avatar already exits for user");
-        address avatar = avatarFactory.newAvatar();
+        address avatar = _deployNewAvatar();
         avatars[msg.sender] = avatar;
         return avatar;
+    }
+
+    /**
+     * @dev Deploys a new instance of Avatar contract
+     */
+    function _deployNewAvatar() internal returns (address) {
+        return address(new Avatar(pool, bComptroller, comptroller, comp, cEther));
     }
 
     function isAvatarExistFor(address user) public view returns (bool) {
--- contracts/bprotocol/interfaces/IAvatarFactory.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity 0.5.16;
-
-interface IAvatarFactory {
-    function newAvatar() external returns (address);
-}
