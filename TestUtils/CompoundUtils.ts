f8b0b259e804381df52a42eb931a7746d44469cc
Jitendra Chittoda <jcchittoda@gmail.com>
2020-07-29 11:46:52 +0530

    Trying to fix CircleCI

--- TestUtils/CompoundUtils.ts
+++ TestUtils/CompoundUtils.ts
@@ -1,21 +1,29 @@
 //import * as compound from "../compound-protocol/networks/development.json";
 
 export class CompoundUtils {
-    public compound = require("../compound-protocol/networks/development.json");
+    public compound: any;
+
+    private loanJson() {
+        this.compound = require("../compound-protocol/networks/development.json");
+    }
 
     public getComptroller(): string {
+        this.loanJson();
         return this.compound.Contracts.Comptroller;
     }
 
     public getPriceOracle(): string {
+        this.loanJson();
         return this.compound.Contracts.PriceOracle;
     }
 
     public getComp(): string {
+        this.loanJson();
         return this.compound.Contracts.COMP;
     }
 
     public getContracts(key: string): string {
+        this.loanJson();
         return this.compound.Contracts[key];
     }
 }
