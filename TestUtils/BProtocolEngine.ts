43bb6429ee03c503d94021acc6e9151c332ae0a1
Jitendra Chittoda <jcchittoda@gmail.com>
2020-07-31 19:57:03 +0530

    Improved BProtocolEngine

--- TestUtils/BProtocolEngine.ts
+++ TestUtils/BProtocolEngine.ts
@@ -4,11 +4,25 @@ import { CompoundUtils } from "./CompoundUtils";
 
 const shell = require("shelljs");
 
+// Compound contracts
+const Comp: t.CompContract = artifacts.require("Comp");
+const Comptroller: t.ComptrollerContract = artifacts.require("Comptroller");
+const PriceOracle: t.PriceOracleContract = artifacts.require("PriceOracle");
+
+// BProtocol contracts
 const Pool: t.PoolContract = artifacts.require("Pool");
 const BComptroller: t.BComptrollerContract = artifacts.require("BComptroller");
 const Registry: t.RegistryContract = artifacts.require("Registry");
-const BToken: t.BTokenContract = artifacts.require("BToken");
+const BEther: t.BEtherContract = artifacts.require("BEther");
+const BErc20: t.BErc20Contract = artifacts.require("BErc20");
 const Avatar: t.AvatarContract = artifacts.require("Avatar");
+
+// Compound class to store all Compound deployed contracts
+export class Compound {
+    public comptroller!: t.ComptrollerInstance;
+    public comp!: t.CompInstance;
+    public priceOracle!: t.PriceOracleInstance;
+}
 
 // BProtocol Class to store all BProtocol deployed contracts
 export class BProtocol {
@@ -16,6 +30,9 @@ export class BProtocol {
     public bComptroller!: t.BComptrollerInstance;
     public registry!: t.RegistryInstance;
     public bTokens: Map<string, t.BTokenInstance> = new Map();
+
+    // variable to hold all Compound contracts
+    public compound!: Compound;
 }
 
 // BProtocol System Engine to manage and deploy BProtocol contracts
@@ -46,6 +63,11 @@ export class BProtocolEngine {
         _bProtocol.registry = await this.deployRegistry();
 
         await _bProtocol.bComptroller.setRegistry(_bProtocol.registry.address);
+
+        _bProtocol.compound = new Compound();
+        _bProtocol.compound.comptroller = await Comptroller.at(this.compoundUtil.getComptroller());
+        _bProtocol.compound.comp = await Comp.at(this.compoundUtil.getComp());
+        _bProtocol.compound.priceOracle = await PriceOracle.at(this.compoundUtil.getPriceOracle());
 
         // console.log("Pool: " + _bProtocol.pool.address);
         // console.log("BComptroller: " + _bProtocol.bComptroller.address);
@@ -82,16 +104,31 @@ export class BProtocolEngine {
     // Child Contract Creation
     // ========================
 
-    // Deploy BToken
-    public async deployNewBToken(symbol: string): Promise<t.BTokenInstance> {
-        const cToken: string = this.compoundUtil.getContracts(symbol);
-        const bToken_addr = await this.bProtocol.bComptroller.newBToken.call(cToken);
-        await this.bProtocol.bComptroller.newBToken(cToken);
-        const bToken: t.BTokenInstance = await BToken.at(bToken_addr);
+    // Deploy BErc20
+    public async deployNewBErc20(symbol: string): Promise<t.BErc20Instance> {
+        const bToken_addr = await this._newBToken(symbol);
+        const bToken: t.BErc20Instance = await BErc20.at(bToken_addr);
         this.bProtocol.bTokens.set(symbol, bToken);
         return bToken;
     }
 
+    // Deploy BEther
+    public async deployNewBEther(symbol: string): Promise<t.BEtherInstance> {
+        const bToken_addr = await this._newBToken(symbol);
+        const bToken: t.BEtherInstance = await BEther.at(bToken_addr);
+        this.bProtocol.bTokens.set(symbol, bToken);
+        return bToken;
+    }
+
+    // Deploy BToken
+    private async _newBToken(symbol: string): Promise<string> {
+        const cToken: string = this.compoundUtil.getContracts(symbol);
+        const bToken_addr = await this.bProtocol.bComptroller.newBToken.call(cToken);
+        await this.bProtocol.bComptroller.newBToken(cToken);
+        return bToken_addr;
+    }
+
+    // Deploy Avatar
     public async deployNewAvatar(_from: string = this.accounts[0]): Promise<t.AvatarInstance> {
         const avatar_addr = await this.bProtocol.registry.newAvatar.call({ from: _from });
         await this.bProtocol.registry.newAvatar({ from: _from });
