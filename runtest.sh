7c0f0bb65d5661fe2379d1ffba51387d6e5989bb
Jitendra Chittoda <jcchittoda@gmail.com>
2020-12-22 18:21:22 +0530

    restarting ganache before every test

--- runtest.sh
+++ runtest.sh
@@ -5,8 +5,13 @@ set -e
 
 # Run each test file individually
 find ./test -type f -name "*.spec.ts" | while read file; do
+  echo "Starting Ganache..."
+  npx hardhat node > /dev/null 2>&1 &
+  export NODE_PID=$!
+  echo "PID: $NODE_PID"
   echo "Running test $file"
   npx hardhat test "$file"
+  kill $NODE_PID
   echo "Sleeping for 10 seconds..."
   sleep 10s
   echo "Done."
