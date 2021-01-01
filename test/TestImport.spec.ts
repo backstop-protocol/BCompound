26e398c0a0d02922e350840e2a585a8e72b73f86
Jitendra Chittoda <jcchittoda@gmail.com>
2021-01-01 09:32:56 +0530

    only code format by VSCode

--- test/TestImport.spec.ts
+++ test/TestImport.spec.ts
@@ -6,7 +6,6 @@ import { BAccounts } from "../test-utils/BAccounts";
 import BN from "bn.js";
 import { toWei } from "web3-utils";
 
-
 const CEther: b.CEtherContract = artifacts.require("CEther");
 const { ZERO_ADDRESS } = require("@openzeppelin/test-helpers/src/constants");
 const { balance, expectEvent, expectRevert } = require("@openzeppelin/test-helpers");
@@ -19,7 +18,9 @@ const BErc20: b.BErc20Contract = artifacts.require("BErc20");
 
 const Import: b.ImportContract = artifacts.require("Import");
 const FlashLoanImport: b.FlashLoanImportContract = artifacts.require("FlashLoanImport");
-const FlashLoanImportWithFees: b.FlashLoanImportWithFeesContract = artifacts.require("FlashLoanImportWithFees");
+const FlashLoanImportWithFees: b.FlashLoanImportWithFeesContract = artifacts.require(
+  "FlashLoanImportWithFees",
+);
 const FlashLoanStub: b.FlashLoanStubContract = artifacts.require("FlashLoanStub");
 
 const chai = require("chai");
@@ -180,7 +181,7 @@ contract("BErc20", async (accounts) => {
       let bFlashLoanImport: b.FlashLoanImportInstance;
       let bFlashLoanImportWithFees: b.FlashLoanImportWithFeesInstance;
       let bFlashLoanStub: b.FlashLoanStubInstance;
-      const flashloanVal = (new BN(10).pow(new BN(18))).mul(new BN(1000));
+      const flashloanVal = new BN(10).pow(new BN(18)).mul(new BN(1000));
       let cZRXBalance = new BN(0);
       let cETHBalance = new BN(0);
       let cBATBalance = new BN(0);
@@ -212,21 +213,23 @@ contract("BErc20", async (accounts) => {
         expect(await cBAT.borrow.call(HUNDRED_BAT, { from: a.user1 })).to.be.bignumber.equal(ZERO);
         await cBAT.borrow(HUNDRED_BAT, { from: a.user1 });
 
-        expect(await cUSDT.borrow.call(FIVE_HUNDRED_USDT, { from: a.user1 })).to.be.bignumber.equal(ZERO);
+        expect(await cUSDT.borrow.call(FIVE_HUNDRED_USDT, { from: a.user1 })).to.be.bignumber.equal(
+          ZERO,
+        );
         await cUSDT.borrow(FIVE_HUNDRED_USDT, { from: a.user1 });
 
         expect(await cZRX.balanceOfUnderlying.call(a.user1)).to.be.bignumber.equal(
           ONE_THOUSAND_ZRX,
         );
-        expect(await cETH.balanceOfUnderlying.call(a.user1)).to.be.bignumber.equal(
-          ONE_ETH,
-        );
+        expect(await cETH.balanceOfUnderlying.call(a.user1)).to.be.bignumber.equal(ONE_ETH);
         expect(await cBAT.balanceOfUnderlying.call(a.user1)).to.be.bignumber.equal(
           ONE_THOUSAND_BAT,
         );
 
         expect(await cBAT.borrowBalanceCurrent.call(a.user1)).to.be.bignumber.equal(HUNDRED_BAT);
-        expect(await cUSDT.borrowBalanceCurrent.call(a.user1)).to.be.bignumber.equal(FIVE_HUNDRED_USDT);
+        expect(await cUSDT.borrowBalanceCurrent.call(a.user1)).to.be.bignumber.equal(
+          FIVE_HUNDRED_USDT,
+        );
 
         // import
         bImport = await Import.new(bProtocol.registry.address, bProtocol.bComptroller.address);
@@ -234,7 +237,7 @@ contract("BErc20", async (accounts) => {
         bFlashLoanImportWithFees = await FlashLoanImportWithFees.new();
         bFlashLoanStub = await FlashLoanStub.new();
 
-        await bFlashLoanStub.deposit({ from : a.user1, value: flashloanVal});
+        await bFlashLoanStub.deposit({ from: a.user1, value: flashloanVal });
 
         // give allowance to import
         cZRXBalance = await cZRX.balanceOf(a.user1);
@@ -260,32 +263,39 @@ contract("BErc20", async (accounts) => {
         expect(await bETH.balanceOf(a.user1)).to.be.bignumber.equal(cETHBalance);
         expect(await bBAT.balanceOf(a.user1)).to.be.bignumber.equal(cBATBalance);
 
-        if(checkDebtAfter) {
+        if (checkDebtAfter) {
           console.log("checking debt after");
           expect(await bBAT.borrowBalanceCurrent.call(a.user1)).to.be.bignumber.equal(HUNDRED_BAT);
-          expect(await bUSDT.borrowBalanceCurrent.call(a.user1)).to.be.bignumber.equal(FIVE_HUNDRED_USDT);
-        }
-        else console.log("skipping checking debt after");
+          expect(await bUSDT.borrowBalanceCurrent.call(a.user1)).to.be.bignumber.equal(
+            FIVE_HUNDRED_USDT,
+          );
+        } else console.log("skipping checking debt after");
 
         console.log("checking delegate was revoked");
         const avatar = await bProtocol.registry.getAvatar.call(a.user1);
         expect(await bProtocol.registry.delegate(avatar, bImport.address)).to.be.equal(false);
       });
 
-
       it("user should import without fees", async () => {
         // call flash import
-        const data = await bFlashLoanImport.contract.methods.flashImport([cZRX.address, cETH.address, cBAT.address],
-                                                             [ZRX.address, "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", BAT.address],
-                                                             [cBAT.address, cUSDT.address],
-                                                             [BAT.address, USDT.address],
-                                                             bImport.address,
-                                                             flashloanVal,
-                                                             bFlashLoanStub.address).encodeABI();
+        const data = await bFlashLoanImport.contract.methods
+          .flashImport(
+            [cZRX.address, cETH.address, cBAT.address],
+            [ZRX.address, "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", BAT.address],
+            [cBAT.address, cUSDT.address],
+            [BAT.address, USDT.address],
+            bImport.address,
+            flashloanVal,
+            bFlashLoanStub.address,
+          )
+          .encodeABI();
 
-        await bProtocol.registry.delegateAndExecuteOnce(bImport.address,
-                                                        bFlashLoanImport.address,
-                                                        data, { from: a.user1 });
+        await bProtocol.registry.delegateAndExecuteOnce(
+          bImport.address,
+          bFlashLoanImport.address,
+          data,
+          { from: a.user1 },
+        );
 
         // check balance
         expect(await bETH.borrowBalanceCurrent.call(a.user1)).to.be.bignumber.equal(ZERO);
@@ -297,17 +307,24 @@ contract("BErc20", async (accounts) => {
         expect(await cETH.borrowBalanceCurrent.call(a.user1)).to.be.bignumber.equal(HALF_ETH);
 
         // call flash import
-        const data = await bFlashLoanImport.contract.methods.flashImport([cZRX.address, cETH.address, cBAT.address],
-                                                             [ZRX.address, "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", BAT.address],
-                                                             [cBAT.address, cUSDT.address, cETH.address],
-                                                             [BAT.address, USDT.address, "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"],
-                                                             bImport.address,
-                                                             flashloanVal,
-                                                             bFlashLoanStub.address).encodeABI();
+        const data = await bFlashLoanImport.contract.methods
+          .flashImport(
+            [cZRX.address, cETH.address, cBAT.address],
+            [ZRX.address, "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", BAT.address],
+            [cBAT.address, cUSDT.address, cETH.address],
+            [BAT.address, USDT.address, "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"],
+            bImport.address,
+            flashloanVal,
+            bFlashLoanStub.address,
+          )
+          .encodeABI();
 
-        await bProtocol.registry.delegateAndExecuteOnce(bImport.address,
-                                                        bFlashLoanImport.address,
-                                                        data, { from: a.user1 });
+        await bProtocol.registry.delegateAndExecuteOnce(
+          bImport.address,
+          bFlashLoanImport.address,
+          data,
+          { from: a.user1 },
+        );
 
         // check balance
         expect(await bETH.borrowBalanceCurrent.call(a.user1)).to.be.bignumber.equal(HALF_ETH);
@@ -315,28 +332,34 @@ contract("BErc20", async (accounts) => {
 
       it("user should import without fees and with no debt", async () => {
         // repay the bat and usdt debt
-        await BAT.approve(cBAT.address, HUNDRED_BAT, { from: a.user1});
-        await cBAT.repayBorrow(HUNDRED_BAT, { from: a.user1});
-        await USDT.approve(cUSDT.address, HUNDRED_BAT, { from: a.user1});
-        await cUSDT.repayBorrow(FIVE_HUNDRED_USDT, { from: a.user1});
+        await BAT.approve(cBAT.address, HUNDRED_BAT, { from: a.user1 });
+        await cBAT.repayBorrow(HUNDRED_BAT, { from: a.user1 });
+        await USDT.approve(cUSDT.address, HUNDRED_BAT, { from: a.user1 });
+        await cUSDT.repayBorrow(FIVE_HUNDRED_USDT, { from: a.user1 });
 
         // make sure borrow balance was reset
         expect(await cBAT.borrowBalanceCurrent.call(a.user1)).to.be.bignumber.equal(ZERO);
         expect(await cUSDT.borrowBalanceCurrent.call(a.user1)).to.be.bignumber.equal(ZERO);
 
-
         // call flash import
-        const data = await bFlashLoanImport.contract.methods.flashImport([cZRX.address, cETH.address, cBAT.address],
-                                                             [ZRX.address, "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", BAT.address],
-                                                             [],
-                                                             [],
-                                                             bImport.address,
-                                                             flashloanVal,
-                                                             bFlashLoanStub.address).encodeABI();
+        const data = await bFlashLoanImport.contract.methods
+          .flashImport(
+            [cZRX.address, cETH.address, cBAT.address],
+            [ZRX.address, "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", BAT.address],
+            [],
+            [],
+            bImport.address,
+            flashloanVal,
+            bFlashLoanStub.address,
+          )
+          .encodeABI();
 
-        await bProtocol.registry.delegateAndExecuteOnce(bImport.address,
-                                                        bFlashLoanImport.address,
-                                                        data, { from: a.user1 });
+        await bProtocol.registry.delegateAndExecuteOnce(
+          bImport.address,
+          bFlashLoanImport.address,
+          data,
+          { from: a.user1 },
+        );
 
         // check balance
         expect(await bETH.borrowBalanceCurrent.call(a.user1)).to.be.bignumber.equal(ZERO);
@@ -348,17 +371,24 @@ contract("BErc20", async (accounts) => {
 
       it("user should import with fees", async () => {
         // call flash import
-        const data = await bFlashLoanImportWithFees.contract.methods.flashImport([cZRX.address, cETH.address, cBAT.address],
-                                                             [ZRX.address, "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", BAT.address],
-                                                             [cBAT.address, cUSDT.address],
-                                                             [BAT.address, USDT.address],
-                                                             bImport.address,
-                                                             flashloanVal,
-                                                             bFlashLoanStub.address).encodeABI();
+        const data = await bFlashLoanImportWithFees.contract.methods
+          .flashImport(
+            [cZRX.address, cETH.address, cBAT.address],
+            [ZRX.address, "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", BAT.address],
+            [cBAT.address, cUSDT.address],
+            [BAT.address, USDT.address],
+            bImport.address,
+            flashloanVal,
+            bFlashLoanStub.address,
+          )
+          .encodeABI();
 
-        await bProtocol.registry.delegateAndExecuteOnce(bImport.address,
-                                                        bFlashLoanImportWithFees.address,
-                                                        data, { from: a.user1 });
+        await bProtocol.registry.delegateAndExecuteOnce(
+          bImport.address,
+          bFlashLoanImportWithFees.address,
+          data,
+          { from: a.user1 },
+        );
 
         // check balance
         expect(await bETH.borrowBalanceCurrent.call(a.user1)).to.be.bignumber.equal(new BN(1000));
@@ -370,20 +400,29 @@ contract("BErc20", async (accounts) => {
         expect(await cETH.borrowBalanceCurrent.call(a.user1)).to.be.bignumber.equal(HALF_ETH);
 
         // call flash import
-        const data = await bFlashLoanImportWithFees.contract.methods.flashImport([cZRX.address, cETH.address, cBAT.address],
-                                                             [ZRX.address, "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", BAT.address],
-                                                             [cBAT.address, cUSDT.address, cETH.address],
-                                                             [BAT.address, USDT.address, "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"],
-                                                             bImport.address,
-                                                             flashloanVal,
-                                                             bFlashLoanStub.address).encodeABI();
+        const data = await bFlashLoanImportWithFees.contract.methods
+          .flashImport(
+            [cZRX.address, cETH.address, cBAT.address],
+            [ZRX.address, "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", BAT.address],
+            [cBAT.address, cUSDT.address, cETH.address],
+            [BAT.address, USDT.address, "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"],
+            bImport.address,
+            flashloanVal,
+            bFlashLoanStub.address,
+          )
+          .encodeABI();
 
-        await bProtocol.registry.delegateAndExecuteOnce(bImport.address,
-                                                        bFlashLoanImportWithFees.address,
-                                                        data, { from: a.user1 });
+        await bProtocol.registry.delegateAndExecuteOnce(
+          bImport.address,
+          bFlashLoanImportWithFees.address,
+          data,
+          { from: a.user1 },
+        );
 
         // check balance
-        expect(await bETH.borrowBalanceCurrent.call(a.user1)).to.be.bignumber.equal(HALF_ETH.add(new BN(1000)));
+        expect(await bETH.borrowBalanceCurrent.call(a.user1)).to.be.bignumber.equal(
+          HALF_ETH.add(new BN(1000)),
+        );
       });
     });
   });
