259fd21cf50ee645c54cfe0678fd3e9931f98189
Jitendra Chittoda <jcchittoda@gmail.com>
2021-01-05 14:21:31 +0530

    fix scoring in transfer and transferFrom

--- contracts/bprotocol/avatar/AbsCToken.sol
+++ contracts/bprotocol/avatar/AbsCToken.sol
@@ -7,6 +7,7 @@ import { ICToken } from "../interfaces/CTokenInterfaces.sol";
 import { ICEther } from "../interfaces/CTokenInterfaces.sol";
 import { ICErc20 } from "../interfaces/CTokenInterfaces.sol";
 import { IScore } from "../interfaces/IScore.sol";
+import { IAvatar } from "../interfaces/IAvatar.sol";
 
 import { Cushion } from "./Cushion.sol";
 
@@ -49,7 +50,7 @@ contract AbsCToken is Cushion {
     // ======
     function mint() public payable onlyBToken postPoolOp(false) {
         cETH.mint.value(msg.value)(); // fails on compound in case of err
-        if(!quit) _score().updateCollScore(address(this), address(cETH), toInt256(msg.value));
+        if(! quit) _score().updateCollScore(address(this), address(cETH), toInt256(msg.value));
     }
 
     function repayBorrow()
@@ -60,7 +61,7 @@ contract AbsCToken is Cushion {
     {
         uint256 amtToRepayOnCompound = _untopPartial(cETH, msg.value);
         if(amtToRepayOnCompound > 0) cETH.repayBorrow.value(amtToRepayOnCompound)(); // fails on compound in case of err
-        if(!quit) _score().updateDebtScore(address(this), address(cETH), -toInt256(msg.value));
+        if(! quit) _score().updateDebtScore(address(this), address(cETH), -toInt256(msg.value));
     }
 
     function liquidateBorrow(ICToken cTokenCollateral) external payable onlyBToken {
@@ -73,7 +74,7 @@ contract AbsCToken is Cushion {
     function mint(ICErc20 cToken, uint256 mintAmount) public onlyBToken postPoolOp(false) returns (uint256) {
         uint result = cToken.mint(mintAmount);
         require(result == 0, "AbsCToken: mint-failed");
-        if(!quit) _score().updateCollScore(address(this), address(cToken), toInt256(mintAmount));
+        if(! quit) _score().updateCollScore(address(this), address(cToken), toInt256(mintAmount));
         return result;
     }
 
@@ -91,7 +92,7 @@ contract AbsCToken is Cushion {
             underlying.safeApprove(address(cToken), repayAmount);
             result = cToken.repayBorrow(amtToRepayOnCompound);
             require(result == 0, "AbsCToken: repayBorrow-failed");
-            if(!quit) _score().updateDebtScore(address(this), address(cToken), -toInt256(repayAmount));
+            if(! quit) _score().updateDebtScore(address(this), address(cToken), -toInt256(repayAmount));
         }
         return result; // in case of err, tx fails at BToken
     }
@@ -110,7 +111,7 @@ contract AbsCToken is Cushion {
         uint256 seizedCTokens = _doLiquidateBorrow(cTokenDebt, underlyingAmtToLiquidate, cTokenCollateral);
         // Convert seizedCToken to underlyingTokens
         uint256 underlyingSeizedTokens = _toUnderlying(cTokenDebt, seizedCTokens);
-        if(!quit) {
+        if(! quit) {
             IScore score = _score();
             score.updateCollScore(address(this), address(cTokenDebt), -toInt256(underlyingSeizedTokens));
             score.updateDebtScore(address(this), address(cTokenCollateral), -toInt256(underlyingAmtToLiquidate));
@@ -128,7 +129,7 @@ contract AbsCToken is Cushion {
         require(result == 0, "AbsCToken: redeem-failed");
 
         uint256 underlyingRedeemAmount = _toUnderlying(cToken, redeemTokens);
-        if(!quit) _score().updateCollScore(address(this), address(cToken), -toInt256(underlyingRedeemAmount));
+        if(! quit) _score().updateCollScore(address(this), address(cToken), -toInt256(underlyingRedeemAmount));
 
         // Do the fund transfer at last
         if(_isCEther(cToken)) {
@@ -149,7 +150,7 @@ contract AbsCToken is Cushion {
         uint256 result = cToken.redeemUnderlying(redeemAmount);
         require(result == 0, "AbsCToken: redeemUnderlying-failed");
 
-        if(!quit) _score().updateCollScore(address(this), address(cToken), -toInt256(redeemAmount));
+        if(! quit) _score().updateCollScore(address(this), address(cToken), -toInt256(redeemAmount));
 
         // Do the fund transfer at last
         if(_isCEther(cToken)) {
@@ -169,8 +170,8 @@ contract AbsCToken is Cushion {
         uint256 result = cToken.borrow(borrowAmount);
         console.log("borrow result: %s", result);
         require(result == 0, "AbsCToken: borrow-failed");
-        
-        if(!quit) _score().updateDebtScore(address(this), address(cToken), toInt256(borrowAmount));
+
+        if(! quit) _score().updateDebtScore(address(this), address(cToken), toInt256(borrowAmount));
 
         // send funds at last
         if(_isCEther(cToken)) {
@@ -190,11 +191,11 @@ contract AbsCToken is Cushion {
         require(result, "AbsCToken: transfer-failed");
 
         uint256 underlyingRedeemAmount = _toUnderlying(cToken, amount);
-        if(!quit) {
-            IScore score = _score();
-            score.updateCollScore(address(this), address(cToken), -toInt256(underlyingRedeemAmount));
-            score.updateCollScore(dstAvatar, address(cToken), toInt256(underlyingRedeemAmount));
-        }
+
+        IScore score = _score();
+        if(! quit) score.updateCollScore(address(this), address(cToken), -toInt256(underlyingRedeemAmount));
+        if(! IAvatar(dstAvatar).quit()) score.updateCollScore(dstAvatar, address(cToken), toInt256(underlyingRedeemAmount));
+
         return result;
     }
 
@@ -206,11 +207,11 @@ contract AbsCToken is Cushion {
         require(result, "AbsCToken: transferFrom-failed");
 
         uint256 underlyingRedeemAmount = _toUnderlying(cToken, amount);
-        if(!quit) {
-            IScore score = _score();
-            score.updateCollScore(srcAvatar, address(cToken), -toInt256(underlyingRedeemAmount));
-            score.updateCollScore(dstAvatar, address(cToken), toInt256(underlyingRedeemAmount));
-        }
+
+        IScore score = _score();
+        if(! IAvatar(srcAvatar).quit()) score.updateCollScore(srcAvatar, address(cToken), -toInt256(underlyingRedeemAmount));
+        if(! IAvatar(dstAvatar).quit()) score.updateCollScore(dstAvatar, address(cToken), toInt256(underlyingRedeemAmount));
+
         return result;
     }
 
