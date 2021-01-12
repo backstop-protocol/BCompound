8b3ae33d27b7196d9ec0fbbee2384f318080a390
Jitendra Chittoda <jcchittoda@gmail.com>
2021-01-12 22:36:25 +0530

    updated shell script to execute all tests and fail at last if any test fails

--- runtest.sh
+++ runtest.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Exit from shell script with an error code if any test fails
-set -e
+failed=0
 
 # Run each test file individually
 find ./test -type f -name "*.spec.ts" | while read file; do
@@ -10,9 +9,27 @@ find ./test -type f -name "*.spec.ts" | while read file; do
   export NODE_PID=$!
   echo "PID: $NODE_PID"
   echo "Running test $file"
-  npx hardhat test "$file"
+  cmd="npx hardhat test $file"
+  $cmd
+  status=$?
+  
+  if [ $? -eq 0 ]
+  then
+    echo "'$cmd' command was successful"
+  else
+    echo "'$cmd' failed"
+    failed=1
+  fi 
+
   kill $NODE_PID
-  echo "Sleeping for 10 seconds..."
-  sleep 10s
+  echo "Sleeping for 5 seconds..."
+  sleep 5s
   echo "Done."
 done
+
+if [ $failed -eq 1 ]
+then
+  exit 1
+else
+  exit 0
+fi
