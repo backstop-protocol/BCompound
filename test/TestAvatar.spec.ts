df16a6e074a2c466c7b2c8ea7b67f627282489c2
Jitendra Chittoda <jcchittoda@gmail.com>
2021-02-02 19:35:46 +0530

    only autofix file format

--- test/TestAvatar.spec.ts
+++ test/TestAvatar.spec.ts
@@ -7,7 +7,6 @@ const { ZERO_ADDRESS } = require("@openzeppelin/test-helpers/src/constants");
 const { expectEvent, expectRevert } = require("@openzeppelin/test-helpers");
 import BN from "bn.js";
 
-
 const chai = require("chai");
 const expect = chai.expect;
 let snapshotId: string;
@@ -15,7 +14,6 @@ const ZERO = new BN(0);
 
 const Avatar: b.AvatarContract = artifacts.require("Avatar");
 const EmergencyMock: b.EmergencyMockContract = artifacts.require("EmergencyMock");
-
 
 contract("Avatar", async (accounts) => {
   let bProtocol: BProtocol;
@@ -50,11 +48,7 @@ contract("Avatar", async (accounts) => {
       expect(await avatar1.registry()).to.be.equal(registry.address);
 
       await expectRevert(
-          avatar1.initialize(
-          a.dummy1,
-          bProtocol.compound.comp.address,
-          engine.getCompVoterAddress(),
-        ),
+        avatar1.initialize(a.dummy1, bProtocol.compound.comp.address, engine.getCompVoterAddress()),
         "avatar-already-init",
       );
 
@@ -85,52 +79,69 @@ contract("Avatar", async (accounts) => {
     });
     it("whitelist emergency1 and set x", async () => {
       const setXCallData = await mockEmergency1.setX.call(777);
-      const sig = setXCallData.substring(0,10); // 2 chars per byte + "0x"
-      await registry.setWhitelistAvatarCall(mockEmergency1.address, sig, true, {from: a.deployer});
-      await avatarInstance.emergencyCall(mockEmergency1.address, setXCallData, {from: a.user1});
+      const sig = setXCallData.substring(0, 10); // 2 chars per byte + "0x"
+      await registry.setWhitelistAvatarCall(mockEmergency1.address, sig, true, {
+        from: a.deployer,
+      });
+      await avatarInstance.emergencyCall(mockEmergency1.address, setXCallData, { from: a.user1 });
       expect(await mockEmergency1.x()).to.be.bignumber.equal(new BN(777));
 
       await expectRevert(
-        avatarInstance.emergencyCall(mockEmergency2.address, setXCallData, {from: a.user1}),
+        avatarInstance.emergencyCall(mockEmergency2.address, setXCallData, { from: a.user1 }),
         "emergencyCall: not-listed",
       );
 
       // delist
-      await registry.setWhitelistAvatarCall(mockEmergency1.address, sig, false, {from: a.deployer});
+      await registry.setWhitelistAvatarCall(mockEmergency1.address, sig, false, {
+        from: a.deployer,
+      });
       await expectRevert(
-        avatarInstance.emergencyCall(mockEmergency1.address, setXCallData, {from: a.user1}),
+        avatarInstance.emergencyCall(mockEmergency1.address, setXCallData, { from: a.user1 }),
         "emergencyCall: not-listed",
       );
     });
 
     it("whitelist emergency1 and set y", async () => {
-      const setYCallData = await mockEmergency1.setY.call(555, {value: "1"});
-      const sig = setYCallData.substring(0,10); // 2 chars per byte + "0x"
+      const setYCallData = await mockEmergency1.setY.call(555, { value: "1" });
+      const sig = setYCallData.substring(0, 10); // 2 chars per byte + "0x"
 
       // before list
-      await registry.setWhitelistAvatarCall(mockEmergency1.address, sig, false, {from: a.deployer});
+      await registry.setWhitelistAvatarCall(mockEmergency1.address, sig, false, {
+        from: a.deployer,
+      });
       await expectRevert(
-        avatarInstance.emergencyCall(mockEmergency1.address, setYCallData, {from: a.user1, value: "1"}),
+        avatarInstance.emergencyCall(mockEmergency1.address, setYCallData, {
+          from: a.user1,
+          value: "1",
+        }),
         "emergencyCall: not-listed",
       );
 
-      await registry.setWhitelistAvatarCall(mockEmergency1.address, sig, true, {from: a.deployer});
-      await avatarInstance.emergencyCall(mockEmergency1.address, setYCallData, {from: a.user1, value: "1"});
+      await registry.setWhitelistAvatarCall(mockEmergency1.address, sig, true, {
+        from: a.deployer,
+      });
+      await avatarInstance.emergencyCall(mockEmergency1.address, setYCallData, {
+        from: a.user1,
+        value: "1",
+      });
       expect(await mockEmergency1.y()).to.be.bignumber.equal(new BN(555));
 
       await expectRevert(
-        avatarInstance.emergencyCall(mockEmergency2.address, setYCallData, {from: a.user1, value: "1"}),
+        avatarInstance.emergencyCall(mockEmergency2.address, setYCallData, {
+          from: a.user1,
+          value: "1",
+        }),
         "emergencyCall: not-listed",
       );
     });
 
     it("quit and set x", async () => {
       const setXCallData = await mockEmergency1.setX.call(777);
-      const sig = setXCallData.substring(0,10); // 2 chars per byte + "0x"
+      const sig = setXCallData.substring(0, 10); // 2 chars per byte + "0x"
 
-      await avatarInstance.quitB({from: a.user1});
+      await avatarInstance.quitB({ from: a.user1 });
 
-      await avatarInstance.emergencyCall(mockEmergency1.address, setXCallData, {from: a.user1});
+      await avatarInstance.emergencyCall(mockEmergency1.address, setXCallData, { from: a.user1 });
       expect(await mockEmergency1.x()).to.be.bignumber.equal(new BN(777));
     });
 
