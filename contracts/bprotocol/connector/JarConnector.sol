516a2672dcc7d3798bffcd769d5e926d6f3465d5
yaron velner <yaron172@yahoo.com>
2021-02-14 13:54:31 +0200

    score progress in jar connector

--- contracts/bprotocol/connector/JarConnector.sol
+++ contracts/bprotocol/connector/JarConnector.sol
@@ -12,42 +12,50 @@ contract JarConnector {
     IRegistry registry;
     address[] public cTokens;
 
-    constructor(address[] memory _cTokens) public {
-        cTokens = _cTokens;
-    }
-
-    function setRegistry(address _registry) public {
+    constructor(address[] memory _cTokens, address _registry) public {
         require(registry == IRegistry(0), "registry-already-set");
         registry = IRegistry(_registry);
         score = IBTokenScore(registry.score());
+
+        cTokens = _cTokens;
     }
 
     function getUserScore(address user) external view returns (uint) {
-        return _getTotalUserScore(user, now, 0);
+        return _getTotalUserScore(user, now);
+    }
+
+    // this ignores the comp speed progress, but should be close enough
+    function getUserScoreProgressPerSec(address user) external view returns (uint) {
+        return _getTotalUserScore(user, now + 1) - _getTotalUserScore(user, now);
     }
 
     function getGlobalScore() external view returns (uint) {
-        return _getTotalGlobalScore(now, 0);
+        return _getTotalGlobalScore(now);
     }
 
-    function _getTotalUserScore(address user, uint time, uint spinStart) internal view returns (uint256) {
+    function _getTotalUserScore(address user, uint time) internal view returns (uint256) {
         uint totalScore = 0;
         for(uint i = 0; i < cTokens.length; i++) {
-            uint debtScore = score.getDebtScore(user, cTokens[i], time, spinStart);
-            uint collScore = score.getCollScore(user, cTokens[i], time, spinStart);
+            uint debtScore = score.getDebtScore(user, cTokens[i], time);
+            uint collScore = score.getCollScore(user, cTokens[i], time);
             totalScore = add_(add_(totalScore, debtScore), collScore);
         }
         return totalScore;
     }
 
-    function _getTotalGlobalScore(uint time, uint spinStart) internal view returns (uint256) {
+    function _getTotalGlobalScore(uint time) internal view returns (uint256) {
         uint totalScore = 0;
         for(uint i = 0; i < cTokens.length; i++) {
-            uint debtScore = score.getDebtGlobalScore(cTokens[i], time, spinStart);
-            uint collScore = score.getCollGlobalScore(cTokens[i], time, spinStart);
+            uint debtScore = score.getDebtGlobalScore(cTokens[i], time);
+            uint collScore = score.getCollGlobalScore(cTokens[i], time);
             totalScore = add_(add_(totalScore, debtScore), collScore);
         }
         return totalScore;
+    }
+
+    function spin() external {
+        require(score.endTime() < now, "too-early");
+        score.spin();
     }
 
     function add_(uint a, uint b) internal pure returns (uint) {
