cf94e5093005a2d7707366b392184fbeedae3d9f
yaron velner <yaron172@yahoo.com>
2021-07-24 23:12:22 +0300

    liquidator info improvement

--- contracts/bprotocol/interfaces/IUniswapAnchoredView.sol
+++ contracts/bprotocol/interfaces/IUniswapAnchoredView.sol
@@ -4,4 +4,5 @@ pragma solidity 0.5.16;
 interface IUniswapAnchoredView {
     function postPrices(bytes[] calldata messages, bytes[] calldata signatures, string[] calldata symbols) external;
     function price(string calldata symbol) external view returns (uint);
+    function getUnderlyingPrice(address cToken) external view returns (uint);
 }
