fed1fde3f0769e557a6b6b3b46a63aa7176d1867
shmuel <shmuel@shmuels-MacBook-Pro.local>
2021-08-23 10:00:40 +0300

    cleanUp

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,6 +10,32 @@ require("@nomiclabs/hardhat-ethers");
 export default {
   defaultNetwork: "localhost",
   networks: {
+    remote: {
+      url: `http://bphardhat-env.eba-raywersi.eu-central-1.elasticbeanstalk.com`,
+      accounts: [
+          "0xc5e8f61d1ab959b397eecc0a37a6517b8e67a0e7cf1f4bce5591f3ed80194444",
+          "0xd49743deccbccc5dc7baa8e69e5be03298da8688a15dd202e20f15d5e0e9a9fb",
+          "0x23c601ae397441f3ef6f1075dcb0031ff17fb079837beadaf3c84d96c6f3e569",
+          "0xee9d129c1997549ee09c0757af5939b2483d80ad649a0eda68e8b0357ad11131",
+          "0x87630b2d1de0fbd5044eb6891b3d9d98c34c8d310c852f98550ba774480e47cc",
+          "0x275cc4a2bfd4f612625204a20a2280ab53a6da2d14860c47a9f5affe58ad86d4",
+          "0x7f307c41137d1ed409f0a7b028f6c7596f12734b1d289b58099b99d60a96efff",
+          "0x2a8aede924268f84156a00761de73998dac7bf703408754b776ff3f873bcec60",
+          "0x8b24fd94f1ce869d81a34b95351e7f97b2cd88a891d5c00abc33d0ec9501902e",
+          "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b29085",
+          "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b29086",
+          "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b29087",
+          "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b29088",
+          "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b29089",
+          "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b2908a",
+          "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b2908b",
+          "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b2908c",
+          "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b2908d",
+          "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b2908e",
+          "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b2908f",
+      ],
+      timeout: 60000
+    },
     hardhat: {
       blockGasLimit: 0xfffffffffff,
       allowUnlimitedContractSize: true,
@@ -38,7 +64,7 @@ export default {
         //   0x68dfc526037e9030c8f813d014919cc89e7d4d74
         //   0x26c43a1d431a4e5ee86cd55ed7ef9edf3641e901
         {
-          privateKey: "0xc5e8f61d1ab959b397eecc0a37a6517b8e67a0e7cf1f4bce5591f3ed80199122",
+          privateKey: "0xc5e8f61d1ab959b397eecc0a37a6517b8e67a0e7cf1f4bce5591f3ed80194444",
           balance: "100000000000000000000000000000000",
         },
         {
